8142d0b020fefa08860fa9699e4be21066ec7b6e
Professor Breaker <jbreak1@ju.edu>
2025-09-11 15:31:08 -0400

    Initial Commit

---
+++ WorldTests/WorldTests.swift
@@ -0,0 +1,17 @@
+//
+//  WorldTests.swift
+//  WorldTests
+//
+//  Created by P.Breaker on 9/11/25.
+//
+
+import Testing
+@testable import World
+
+struct WorldTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
