34939604d135628586ad99303e791aa72fedea9f
Professor Breaker <jbreak1@ju.edu>
2025-09-18 18:21:32 -0400

    temp conversion

--- World/ConversionViewController.swift
+++ World/ConversionViewController.swift
@@ -10,20 +10,53 @@ import UIKit
 class ConversionViewController: UIViewController {
     @IBOutlet var celsiusLabel: UILabel!
     @IBOutlet var textField: UITextField!
+    
+    var fahrenheitValue: Measurement<UnitTemperature>? {
+        didSet {
+            updateCelsiusLabel()
+        }
+    }
+    
+    let numberFormatter: NumberFormatter = {
+        let nf = NumberFormatter()
+        nf.numberStyle = .decimal
+        nf.minimumFractionDigits = 0
+        nf.maximumFractionDigits = 1
+        return nf
+    }()
+    
+    var celsiusValue: Measurement<UnitTemperature>? {
+        if let fahrenheitValue = fahrenheitValue {
+            return fahrenheitValue.converted(to: .celsius)
+        } else {
+            return nil
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
 
            print("ConversionViewController loaded its view.")
+        
+        updateCelsiusLabel()
+    }
+    
+    func updateCelsiusLabel() {
+        if let celsiusValue = celsiusValue {
+            celsiusLabel.text =
+                        numberFormatter.string(from: NSNumber(value: celsiusValue.value))
+        } else {
+            celsiusLabel.text = "???"
+        }
     }
 
     @IBAction func fahrenheitFieldEditingChanged(_ textField: UITextField) {
         // Set C Text field
-        if let text = textField.text, !text.isEmpty {
-                celsiusLabel.text = text
+        if let text = textField.text, let value = Double(text) {
+                fahrenheitValue = Measurement(value: value, unit: .fahrenheit)
             } else {
-                celsiusLabel.text = "???"
+                fahrenheitValue = nil
             }
         }
     
